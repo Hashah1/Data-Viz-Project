97116bbe77a5c14308b229caa1fd330b2df61a5f
Mian Hashim Shah <mian_hashim_shah@hotmail.com>
2020-07-23 18:58:23 -0700

    Reiterated requirements for project - didnt need react. Removed react and replaced with html approach

---
+++ index.js
@@ -0,0 +1,17 @@
+// Create SVG Canvas
+const w = 800
+const h = 500
+const svg = d3.select("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .attr("align", "center")
+    .style("border", "1px solid black")
+
+// Extract Data
+async function getData() {
+    // const urbanData = await d3.csv()
+    // const ruralData = await d3.csv
+}
+// Create Bars
+
+// Axis, scales
