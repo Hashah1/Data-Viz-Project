d0606dc1afc1583e54a61c9a4db5ae2c1842273b
Mian Hashim Shah <mian_hashim_shah@hotmail.com>
2020-07-23 19:25:46 -0700

    Testing init()

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ const svg = d3.select("svg")
     .attr("align", "center")
     .style("border", "1px solid black")
 
+console.log("here")
 // Extract Data
-async function getData() {
+async function init() {
+    // const urbanData = await d3.csv('PercentageAccessUrban.csv')
+    const data = await d3.csv('https//flunky.github.io/data.csv');
+    console.log("here", data)
     // const urbanData = await d3.csv()
-    // const ruralData = await d3.csv
+    const ruralData = await d3.csv('https://github.com/Hashah1/data-viz-project/blob/master/PercentageAccessRural.csv')
+    console.log(urbanData)
+    console.log(ruralData)
 }
 // Create Bars
 
